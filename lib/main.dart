6e0b254cd026d17ba65f56596698e79eb2f9f6f5
zhuikovda <dmitiy.zhuikov@janix-soft.com>
2023-12-03 08:56:12 +0300

    refactor: refactor

--- lib/main.dart
+++ lib/main.dart
@@ -10,38 +10,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Colors.teal,
-          body: SafeArea(
-              child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage('images/programmer.jpeg'),
-                ),
-                Text(
-                  'Dima Zh',
-                  style: TextStyle(
-                      fontSize: 40.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Lobster'),
-                ),
-                Text(
-                  'Flutter developer'.toUpperCase(),
-                  style: TextStyle(
-                      fontSize: 26.0,
-                      color: Colors.teal.shade100,
-                      letterSpacing: 2.5,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Source Sans 3'),
-                ),
-              ],
-            ),
-          )),
-        ));
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/programmer.jpeg'),
+              ),
+              Text(
+                'Dima Zh',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Lobster'),
+              ),
+              Text(
+                'Flutter developer'.toUpperCase(),
+                style: TextStyle(
+                    fontSize: 26.0,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Source Sans 3'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
